6b1dc5b53ae55e3817c6f79703c44077d642c6be
G Chandra Maheswara Reddy (Attra) <chandra.reddy@attra.com.au>
2018-05-12 15:32:14 +0530

    msd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { FeesWaiversComponent } from './fees-waivers/fees-waivers.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { YearlyReportsComponent } from './yearly-reports/yearly-reports.component';
 import { MonthlyReportsComponent } from './monthly-reports/monthly-reports.component';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
 
 
 
@@ -40,10 +41,10 @@ const appRoutes: Routes = [{ path: 'Emps', component: AppComponent },
   { path: 'login', component: LoginComponent },
   { path: 'land', component: LaunchComponent},
   { path: 'demographic', component: DemographicComponent},
-  { path: 'summary', component: SummaryComponent},
+  // { path: 'summary', component: SummaryComponent},
   { path: 'recentTransactions', component: RecentTransactionsComponent},
   { path: 'listCards', component: ListCardsComponent},
-  { path: 'transactions', component: TransactionsComponent},
+  // { path: 'transactions', component: TransactionsComponent},
   { path: 'selfLimit', component: SelfLimitComponent},
   { path: 'supplementaryLimit', component: SupplementaryLimitComponent},
   // { path: 'statement', component: StatementComponent},
@@ -65,16 +66,16 @@ const appRoutes: Routes = [{ path: 'Emps', component: AppComponent },
   { path: 'Home', component: HomeComponent,children:
   [
     {
-      path: 'loans', component: LoansComponent, outlet:'Accounts'
+      path: 'profileInfo', component: ProfileInfoComponent, outlet:'Accounts'
     },
     {
-    path: 'accounts', component: AccountsComponent, outlet:'Accounts'
+    path: 'Summary', component: SummaryComponent, outlet:'Accounts'
     },
     {
-      path: 'deposits', component: DepositsComponent, outlet:'Accounts'
+      path: 'transactions', component: TransactionsComponent, outlet:'Accounts'
     },
     {
-      path: 'creditCards', component: CreditCardsComponent, outlet:'Accounts'
+      path: 'FeesWaivers', component: FeesWaiversComponent, outlet:'Accounts'
     }
     
 ]
@@ -177,7 +178,8 @@ const appRoutes: Routes = [{ path: 'Emps', component: AppComponent },
     FeesWaiversComponent,
     NotificationsComponent,
     YearlyReportsComponent,
-    MonthlyReportsComponent   
+    MonthlyReportsComponent,
+    ProfileInfoComponent   
   ],
   imports: [
     BrowserModule,FormsModule,RouterModule.forRoot(appRoutes)
