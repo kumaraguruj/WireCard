8b3a0555ed2724ea7a06792547fd663ec2a7e06b
Premananada Routray (Attra) <premananada.routray@attra.com.au>
2018-05-15 11:17:31 +0530

    3d pie chart added

--- src/app/yearly-reports/yearly-reports.component.ts
+++ src/app/yearly-reports/yearly-reports.component.ts
@@ -15,47 +15,38 @@ export class YearlyReportsComponent implements OnInit {
   constructor(private  AmCharts:AmChartsService) { }
   ngAfterViewInit() {
     
-    this.chart = this.AmCharts.makeChart("chartdiv", {
-  "type": "pie",
-      "theme": "light",
-      "titles": [ {
-        "text": "Expenses By Category",
-        "size": 16
-      } ],
-      "dataProvider": [{
-          "category": "Grossory & Shopping",
-          "expenditure": 500.90
-      }, {
-          "category": "Resturants",
-          "expenditure": 301.90
-      }, {
-          "category": "Fuel",
-          "expenditure": 200.10
-      }, {
-          "category": "E-Commerce",
-          "expenditure": 165.10
-      }, {
-          "category": "Transfer to Others' Account",
-          "expenditure": 1200
-      }, {
-          "category": "Miscellanous",
-          "expenditure": 132
-      }],
-      "valueField": "expenditure",
-      "titleField": "category",
-      "startEffect": "elastic",
-      "startDuration": 2,
-      "labelRadius": 15,
-      "innerRadius": "50%",
-      "depth3D": 10,
-      "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
-      "angle": 15,
-      "export": {
+    this.chart = this.AmCharts.makeChart( "chartdiv", {
+        "type": "pie",
+        "theme": "none",
+        "dataProvider": [{
+            "category": "Grossory & Shopping",
+            "expenditure": 500.90
+        }, {
+            "category": "Resturants",
+            "expenditure": 301.90
+        }, {
+            "category": "Fuel",
+            "expenditure": 200.10
+        }, {
+            "category": "E-Commerce",
+            "expenditure": 165.10
+        }, {
+            "category": "Transfer to Others' Account",
+            "expenditure": 1200
+        }, {
+            "category": "Miscellanous",
+            "expenditure": 132
+        }],
+        "valueField": "expenditure",
+        "titleField": "category",
+        "outlineAlpha": 0.4,
+        "depth3D": 15,
+        "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+        "angle": 30,
+        "export": {
           "enabled": true
-  
-      }
-    
-    });
+        }
+      } );
   }
   ngOnInit() {
 
