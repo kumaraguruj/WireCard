4fd4b54f086f712c456d75b11babb3bc8b343786
Vishwanath Hegde (Attra) <vishwanath.hegde@attra.com.au>
2018-06-06 14:41:25 +0530

    Upgrade Request Slide changes

--- src/app/upgrade-request/upgrade-request.component.ts
+++ src/app/upgrade-request/upgrade-request.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef, Inject } from '@angular/core';
 import {InformationService } from '../Common/information.service';
-
+declare var jQuery:any;
 @Component({
   selector: 'app-upgrade-request',
   templateUrl: './upgrade-request.component.html',
   styleUrls: ['./upgrade-request.component.css']
 })
 export class UpgradeRequestComponent implements OnInit {
+  elementRef: ElementRef;
+    slideValue: number;
+
   LinksFlag:boolean=false;
-  constructor() { 
+  constructor(@Inject(ElementRef) elementRef: ElementRef) { 
+    this.elementRef = elementRef;
     this.LinksFlag=InformationService.UserType==='CreditUser'?false:true;
   
   }
@@ -16,6 +20,18 @@ export class UpgradeRequestComponent implements OnInit {
     alert("Request for upgrade submitted successfully");
   }
   ngOnInit() {
+
+    jQuery(this.elementRef.nativeElement).find("#amount").slider({
+      range: false,
+      orientation: "vertical",
+      min: 0,
+      max: 200000,
+      value: 200000,
+      slide: function( event, ui ) {
+        this.slideValue = ui.value; //doesn't seem to work
+        jQuery(this.elementRef.nativeElement)( "#amount" ).val( ui.value ); 
+      }
+    });
   }
 
 }
